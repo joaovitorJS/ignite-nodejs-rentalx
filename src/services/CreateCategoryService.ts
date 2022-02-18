098f1007c4deca6d1eae380eee82c3aef1bad65f
Joao Vitor <joao.vitordeoliveira@hotmail.com>
2022-02-18 14:53:14 -0300

    Refatorando e usando alguns princípios SOLID

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,28 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+/**
+ * [x] - Definir o tipo de retorno
+ * [x] - Alterar o retorno de erro
+ * [x] - Acessar o repositório
+ */
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    /* verifica se já existe uma categoria cadastrada com o nome passado */
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category alreary exists!");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
